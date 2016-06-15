f14ceace80d4868e2a903f74057577b7a9057201
Artem Lyksa <artemlyksa@gmail.com>
2016-06-15 18:16:18 +0300

    final commit

--- Papers/PapersFlowLayout.swift
+++ Papers/PapersFlowLayout.swift
@@ -9,22 +9,33 @@
 import UIKit
 
 class PapersFlowLayout: UICollectionViewFlowLayout {
-  
-  var appearingIndexPath: NSIndexPath?
-  
-  override func initialLayoutAttributesForAppearingItemAtIndexPath(itemIndexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
     
-    guard let attributes = super.initialLayoutAttributesForAppearingItemAtIndexPath(itemIndexPath),
-      indexPath = appearingIndexPath where indexPath == itemIndexPath  else {
-        return nil
+    var appearingIndexPath: NSIndexPath?
+    var disappearingItemsIndexPaths: [NSIndexPath]?
+    
+    
+    override func initialLayoutAttributesForAppearingItemAtIndexPath(itemIndexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
+        
+        guard let attributes = super.initialLayoutAttributesForAppearingItemAtIndexPath(itemIndexPath),
+            indexPath = appearingIndexPath where indexPath == itemIndexPath  else {
+                return nil
+        }
+        
+        attributes.alpha = 1.0
+        attributes.center = CGPoint(x: CGRectGetWidth(collectionView!.frame) - 23.5, y: -24.5)
+        attributes.transform = CGAffineTransformMakeScale(0.15, 0.15)
+        attributes.zIndex = 99
+        
+        return attributes
     }
-    
-    attributes.alpha = 1.0
-    attributes.center = CGPoint(x: CGRectGetWidth(collectionView!.frame) - 23.5, y: -24.5)
-    attributes.transform = CGAffineTransformMakeScale(0.15, 0.15)
-    attributes.zIndex = 99
-    
-    return attributes
-  }
-  
+    override func finalLayoutAttributesForDisappearingItemAtIndexPath(itemIndexPath: NSIndexPath) -> UICollectionViewLayoutAttributes? {
+        guard let attributes = super.finalLayoutAttributesForDisappearingItemAtIndexPath(itemIndexPath),
+            indexPaths = disappearingItemsIndexPaths where indexPaths.contains(itemIndexPath) else {
+                return nil
+        }
+        attributes.alpha = 1.0
+        attributes.transform = CGAffineTransformMakeScale(0.1, 0.1)
+        attributes.zIndex = -1
+        return attributes
+    }
 }
